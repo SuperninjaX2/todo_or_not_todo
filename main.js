37bf084b46cb58ced906baac15bd19c715d5c5fb
SuperninjaX2 <80552583+SuperninjaX2@users.noreply.github.com>
2023-09-14 22:31:49 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,52 +1,58 @@
-let btn = document.querySelector(".add")
-let box = document.querySelector(".content")
+let box = document.querySelector(".content");
 
-function getValue(){
+function getValue() {
   let value = document.getElementById("note").value;
-    return value
+  return value;
 }
-clear=()=>{
-  let textArea = document.querySelector("textarea")
-  textArea.value =""
+
+clear = () => {
+  let textArea = document.querySelector("textarea");
+  textArea.value = "";
+};
+
+function save(value) {
+  var key = "data_" + Date.now();
+  localStorage.setItem(key, value);
+  return key;
 }
-function insertElements(value){
-  localStorage.setItem("value",value)
-  if(value !=""){
-  let div = document.createElement("div")
-  div.classList.add("box")
-  box.appendChild(div)
-  
-  let p = document.createElement("p")
-  p.classList.add("text")
-  div.appendChild(p)
-  p.innerHTML=value;
-  
-  let deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("btn-delete");
-  deleteBtn.innerHTML="X"
-  div.appendChild(deleteBtn)
-  deleteBtn.addEventListener("click",function(){
-    div.remove()
-    localStorage.removeItem("value")
-  })
-  clear()
+
+function insertElements(key, value) {
+  if (value !== "") {
+    let div = document.createElement("div");
+    div.classList.add("box");
+    box.appendChild(div);
+
+    let p = document.createElement("p");
+    p.classList.add("text");
+    div.appendChild(p);
+    p.innerHTML = value;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("btn-delete");
+    deleteBtn.innerHTML = "X";
+    div.appendChild(deleteBtn);
+    deleteBtn.addEventListener("click", function () {
+      div.remove();
+      localStorage.removeItem(key); // Remove the item from localStorage using the key
+    });
+    clear();
+  }
 }
-}
-window.onload = function() {
-  // Check if there is data in localStorage
-  if (localStorage.getItem('value')) {
-    // Data exists in localStorage, do something with it
-    var data = localStorage.getItem('value');
-    // Your code here to handle the data
-    insertElements(data)
-  } else {
-    // No data found in localStorage
-    // Your code for the case when there's no data
+
+window.onload = function () {
+  // Loop through all items in localStorage and display them
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let data = localStorage.getItem(key);
+    insertElements(key, data);
   }
 };
 
-function ADD(){
-insertElements(getValue())
+function ADD() {
+  let value = getValue();
+  let key = save(value);
+  insertElements(key, value);
 }
 
-btn.addEventListener("click",ADD)
+let btn = document.querySelector(".add");
+btn.addEventListener("click", ADD);
