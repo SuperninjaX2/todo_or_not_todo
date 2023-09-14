075048605705de551be1d8ebc3abffdaee101b35
SuperninjaX2 <80552583+SuperninjaX2@users.noreply.github.com>
2023-09-14 19:23:30 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 let btn = document.querySelector(".add")
-
-
 let box = document.querySelector(".content")
 
 function getValue(){
   let value = document.getElementById("note").value;
     return value
 }
-
+clear=()=>{
+  let textArea = document.querySelector("textarea")
+  textArea.value =""
+}
 function insertElements(value){
+  localStorage.setItem("value",value)
   if(value !=""){
-    
   let div = document.createElement("div")
   div.classList.add("box")
   box.appendChild(div)
@@ -26,9 +27,24 @@ function insertElements(value){
   div.appendChild(deleteBtn)
   deleteBtn.addEventListener("click",function(){
     div.remove()
+    localStorage.removeItem("value")
   })
+  clear()
 }
 }
+window.onload = function() {
+  // Check if there is data in localStorage
+  if (localStorage.getItem('value')) {
+    // Data exists in localStorage, do something with it
+    var data = localStorage.getItem('value');
+    // Your code here to handle the data
+    insertElements(data)
+  } else {
+    // No data found in localStorage
+    // Your code for the case when there's no data
+  }
+};
+
 function ADD(){
 insertElements(getValue())
 }
